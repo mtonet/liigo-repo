114e5803a0cc3c2ca854dd944e830339f564972e
rcdesigners <webmaster@rcdesigners.com.br>
2018-03-23 18:19:59 -0300

    Atualização anuncio

--- includes-acoes/usuario/img-pq.php
+++ includes-acoes/usuario/img-pq.php
@@ -4,7 +4,7 @@ $codfot=rand("1","1234567890");
 $arquivoname="$codfot$arquivo01_nome";
 copy($arquivo01_temporario, "uploads/usuarios/$arquivoname");
 // Chama o arquivo com a classe WideImage  
-require"admin/includes-acoes/lib/WideImage.inc.php"; 
+require"office/includes-acoes/lib/WideImage.inc.php"; 
 // Carrega a imagem a ser manipulada  
 $image=wiImage::load("uploads/usuarios/$arquivoname"); 
 // Redimensiona a imagem  
